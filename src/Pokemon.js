2b8f7ea43f8c2eaa324feea6d04311a0e0b01d4e
Felipe Cardoso <felipedclc@gmail.com>
2021-02-23 18:28:22 -0300

    exercicio pokedex / botoes para passar os pokemons

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 class Pokemon extends React.Component {
     render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
+        const { pokemon } = this.props;
+        const { name, type, averageWeight, image } = pokemon;
 
         return (
             <div className="pokemon">
